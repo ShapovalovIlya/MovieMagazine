a519283bfb706beeea9801ffde45d2a340364cc5
Ilia Shapovalov <greateman@mail.ru>
2023-10-28 22:06:49 +0300

    Initial Commit

---
+++ MovieMagazine/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MovieMagazine
+//
+//  Created by Илья Шаповалов on 28.10.2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
